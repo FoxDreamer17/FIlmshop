19f54e5f48734e096330f216ee1220dabf279a7d
Headshot20072007@mail.ru <headshot20072007@mail.ru>
2021-06-02 16:48:08 +0300

    Магазин с витриной

--- main.rb
+++ main.rb
@@ -2,7 +2,23 @@ require_relative 'lib/product'
 require_relative 'lib/movie'
 require_relative 'lib/book'
 
-Matrix = Movie.new(price: 990, amount: 5)
+products = []
 
-puts "Фильм Матрица стоит #{Matrix.price} рублей"
+products << Movie.new(
+  title: 'Матрица', year:'1999', director:"Джоэл Сильвер", price: 1000, amount: 20
+)
+products << Movie.new(
+  title: 'Дурак', year: '2014', director: 'Юрий Быков', price: 390, amount: 1
+)
 
+products << Book.new(
+  title: 'Идиот',
+  genre: 'роман',
+  author: 'Федор Достоевский',
+  price: 1500,
+  amount: 10
+)
+
+puts 'Вот какие товары у нас есть:'
+puts
+products.each { |product| puts product }

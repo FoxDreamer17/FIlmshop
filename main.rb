7804869dec51c88930b7ac82e1e1a7ec4efe885b
Headshot20072007@mail.ru <headshot20072007@mail.ru>
2021-06-04 12:25:03 +0300

    Final Books/Films Shop

--- main.rb
+++ main.rb
@@ -1,18 +1,10 @@
 require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/movie'
+require_relative 'lib/product_collection'
 
-current_path = File.dirname(__FILE__)
-movie = Movie.from_file(current_path + '/data/films/01.txt')
-book = Book.from_file(current_path + '/data/books/01.txt')
-
-
-# Выведем результат на экран
-puts movie
-puts book
-
-begin
-  Product.from_file(current_path + '/data/films/01.txt')
-rescue NotImplementedError
-  puts 'Метод класса Product.from_file не реализован'
+collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
+collection.sort!(by: :price, order: :asc)
+collection.to_a.each do |product|
+  puts product
 end
